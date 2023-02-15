323c9f4ecaf2f9925b17e13c7bf8e053d7a72c56
wanglinglei <18627024279@163.com>
2023-02-15 21:10:15 +0800

    feat: runtime-code 简单diff算法

--- packages/runtime-core/src/render.ts
+++ packages/runtime-core/src/render.ts
@@ -35,7 +35,12 @@ export function createRender(renderOptionDOM: IRenderOptionDOM) {
    * @param {*} container  节点挂载的容器
    * @return {*}
    */
-  const patch = (n1: TPatchN, n2: TPatchN, container: HTMLElement) => {
+  const patch = (
+    n1: TPatchN,
+    n2: TPatchN,
+    container: HTMLElement,
+    anchor: HTMLElement | null = null
+  ) => {
     // 判断是否是同一个元素 1:不是替换  2. 是 比对属性 子节点等
     console.log("isSameNode", n1 && !isSameNode(n1, n2), n1, n2);
 
@@ -57,7 +62,7 @@ export function createRender(renderOptionDOM: IRenderOptionDOM) {
       default:
         if (shapeFlag & ShapeFlags.ELEMENT) {
           // 是元素
-          processElement(n1, n2, container);
+          processElement(n1, n2, container, anchor);
         } else if (shapeFlag & ShapeFlags.STATEFUL_COMPONENT) {
           // 组件
           processComponent(n1, n2, container);
@@ -113,18 +118,125 @@ export function createRender(renderOptionDOM: IRenderOptionDOM) {
   };
 
   /**
+   * @description: 比对子元素
+   * @description :四种情况 1. 旧有新没有 2.新有就没有 3. 都有 都为文本  4. 都有 为数组
+   * @param {IVnode} n1
+   * @param {IVnode} n2
+   * @param {HTMLElement} el
+   * @return {*}
+   */
+  const patchChild = (n1: IVnode, n2: IVnode, el: HTMLElement) => {
+    const c1 = n1.children;
+    const c2 = n2.children;
+
+    // 都为文本
+    const prevShapeFlag = n1.shapeFlag;
+    const nextShapeFlag = n2.shapeFlag;
+    if (nextShapeFlag & ShapeFlags.TEXT_CHILDREN) {
+      // @todo 卸载子节点
+      setElementText(el, c2 as string);
+    } else {
+      // 新的为数组
+      if (prevShapeFlag & ShapeFlags.ARRAY_CHILDREN) {
+        // 新旧都为数组
+        patchChildren(c1 as [], c2 as [], el);
+      } else {
+        // 旧的为文本
+        setElementText(el, "");
+        mountChildren(el, c2 as string[]);
+      }
+    }
+  };
+
+  /**
+   * @description: 子元素diff
+   * @param {any} c1
+   * @param {any} c2
+   * @param {HTMLElement} el
+   * @return {*}
+   */
+  const patchChildren = (c1: IVnode[], c2: IVnode[], el: HTMLElement) => {
+    let i = 0,
+      len1 = c1.length - 1,
+      len2 = c2.length - 1;
+
+    // 先从头部开始比对   1.同一位置比对 元素不同停止/ 某一个数组结束停止
+    // old : <div>  <p></p> </div>
+    // new : <div>  <p></p>  <div></div>      </div
+    while (i <= len1 && i <= len2) {
+      const n1 = c1[i];
+      const n2 = c2[i];
+      if (isSameNode(n1, n2)) {
+        patch(n1, n2, el);
+      } else {
+        break;
+      }
+      i++;
+    }
+
+    // 从尾部开始
+    while (i <= len1 && i <= len2) {
+      const n1 = c1[len1];
+      const n2 = c2[len2];
+      if (isSameNode(n1, n2)) {
+        patch(n1, n2, el);
+      } else {
+        break;
+      }
+      len1--;
+      len2--;
+    }
+    console.log("patch-children", i, len1, len2);
+
+    //  1. 旧的数据少 新的数据多  2. 旧的数据多 新的数据少
+    if (i > len1) {
+      // 旧的数据少 新的数据多
+      const nextNode = len2 + 1; // 插入节点的位置
+      const anchor = nextNode < c2.length ? c2[nextNode].el : null;
+      while (i <= len2) {
+        patch(null, c2[i++], el, anchor);
+      }
+    } else if (i > len2) {
+      // 旧的比新的多 删除多余的
+      while (i <= len1) {
+        unmount(c1[i]);
+        i++;
+      }
+    } else {
+      // 中间乱序的情况
+      // 对比新旧 分别 找到可以复用的节点  新增的节点  删除的节点
+      const s1 = i,
+        s2 = i;
+
+      let keyIndexMap = new Map();
+      for (let i = s2; i <= len2; i++) {
+        const childNode = c2[i];
+        keyIndexMap.set(childNode["key"], childNode);
+      }
+    }
+  };
+
+  /**
    * @description:  更新元素
    * @param {IVnode} n1
-   * @param {TPatchN} n2
+   * @param {IVnode} n2
    * @param {HTMLElement} containder
    * @return {*}
    */
-  const patchElement = (n1: IVnode, n2: TPatchN, containder: HTMLElement) => {
+  const patchElement = (
+    n1: IVnode,
+    n2: IVnode,
+    container: HTMLElement,
+    anchor: HTMLElement | null = null
+  ) => {
     let el = ((n2 as IVnode).el = n1.el);
     // 先处理属性
     const oldProps = n1?.props || {};
     const newProps = n2?.props || {};
     patchElementProps(el, oldProps, newProps);
+
+    // 处理子节点
+    patchChild(n1, n2, el);
   };
   /**
    * @description: 处理元素
@@ -133,12 +245,17 @@ export function createRender(renderOptionDOM: IRenderOptionDOM) {
    * @param {HTMLElement} container
    * @return {*}
    */
-  const processElement = (n1: TPatchN, n2: TPatchN, container: HTMLElement) => {
+  const processElement = (
+    n1: TPatchN,
+    n2: IVnode,
+    container: HTMLElement,
+    anchor: HTMLElement | null = null
+  ) => {
     if (n1 === null) {
-      mountElement(n2, container);
+      mountElement(n2, container, anchor);
     } else {
       // 同一个元素更新 比对属性 子元素
-      patchElement(n1, n2, container);
+      patchElement(n1, n2, container, anchor);
     }
   };
 
@@ -148,7 +265,11 @@ export function createRender(renderOptionDOM: IRenderOptionDOM) {
    * @param {HTMLElement} container
    * @return {*}
    */
-  const mountElement = (n2: TPatchN, container: HTMLElement) => {
+  const mountElement = (
+    n2: TPatchN,
+    container: HTMLElement,
+    anchor: HTMLElement | null = null
+  ) => {
     // 递归渲染 =>dom 操作 => 渲染到指定位置
     if (n2) {
       const { shapeFlag, type, props, children } = n2;
@@ -170,7 +291,7 @@ export function createRender(renderOptionDOM: IRenderOptionDOM) {
         }
       }
       //插入到对应位置
-      insert(ele, container);
+      insert(ele, container, anchor);
     }
   };
 
